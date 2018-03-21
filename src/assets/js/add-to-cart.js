348ee594ef978c2a29ae9c0ca57c2988726e3a98
Joe Allen <joe@jumprope.design>
2018-03-21 11:20:12 -0400

    Commit files for Foundation demo page

---
+++ src/assets/js/add-to-cart.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+
+/* name: addToCart */
+/* desc: Do something after 'Add to Cart' is clicked */
+
+/* class */
+var addToCart =
+{
+	/* properties */
+
+	/* methods */
+	init: function()
+	{
+		console.log('workin...');
+	}
+};
+
+
+$(document).ready( function(){ addToCart.init(); });
